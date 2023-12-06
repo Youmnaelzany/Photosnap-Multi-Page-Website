351558e1352ede1c4d36a810e766b425fc722e30
Yaya <youmnael38@gmail.com>
2023-12-06 14:47:47 +0200

    finished header and first 2 sections
    finished header and first 2 sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,20 +8,20 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
+    fontFamily: {
+      'dm-sans': ['DM Sans', 'sans-serif']
+    },
     container: {
       center: true,
       padding: "2rem",
       screens: {
         "2xl": "1400px",
       },
-      colors: {
-        'pure-black': '#000000',
-        'light-gray': '#DFDFDF',
-        'pure-white': '#FFFFFF',
-      },
-      fontFamily: {
-        'dm-sans': ['DM Sans', 'sans-serif']
-      }
+    },
+    colors: {
+      'pure-black': '#000000',
+      'light-gray': '#DFDFDF',
+      'pure-white': '#FFFFFF',
     },
     extend: {
     },
